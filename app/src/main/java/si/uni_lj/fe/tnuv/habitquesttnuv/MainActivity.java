2425cc5cd89ebf53a730cfc901f3b0f747108a85
nejcmih <nejcmihelic10@gmail.com>
2024-05-27 00:12:27 +0200

    Osnovni meni

--- app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/MainActivity.java
@@ -1,14 +1,52 @@
 package si.uni_lj.fe.tnuv.habitquesttnuv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+
+public class MainActivity extends AppCompatActivity implements OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.menu_good);
+    }
+    GoodHabitFragment goodHabitFragment = new GoodHabitFragment();
+    BadHabitFragment badHabitFragment = new BadHabitFragment();
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId() == R.id.menu_good) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, goodHabitFragment)
+                    .commit();
+            return true;
+        }
+        else if (item.getItemId() == R.id.menu_bad) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, badHabitFragment)
+                        .commit();
+                return true;
+        }
+        return false;
     }
 }
