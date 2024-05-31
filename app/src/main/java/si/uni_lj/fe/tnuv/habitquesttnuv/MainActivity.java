165c2364a0b74b39186ceaa4660dfb491553695e
nejcmih <nejcmihelic10@gmail.com>
2024-05-31 12:37:32 +0200

    Status bar na vrhu v barvi ozadja

--- app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/MainActivity.java
@@ -1,5 +1,6 @@
 package si.uni_lj.fe.tnuv.habitquesttnuv;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,8 @@ import androidx.fragment.app.FragmentTransaction;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
@@ -16,10 +19,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavig
 public class MainActivity extends AppCompatActivity implements OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
+    GoodHabitFragment goodHabitFragment = new GoodHabitFragment();
+    BadHabitFragment badHabitFragment = new BadHabitFragment();
+    DailyTasksFragment dailyTasksFragment = new DailyTasksFragment();
+    EventRaidFragment eventRaidFragment = new EventRaidFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -28,12 +38,6 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.menu_good);
     }
-
-    GoodHabitFragment goodHabitFragment = new GoodHabitFragment();
-    BadHabitFragment badHabitFragment = new BadHabitFragment();
-    DailyTasksFragment dailyTasksFragment = new DailyTasksFragment();
-    EventRaidFragment eventRaidFragment = new EventRaidFragment();
-    ProfileFragment profileFragment = new ProfileFragment();
 
     public boolean onNavigationItemSelected(@NonNull MenuItem item)
     {
@@ -45,23 +49,23 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
             return true;
         }
         else if (item.getItemId() == R.id.menu_bad) {
-                getSupportFragmentManager()
+            getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.frame_layout, badHabitFragment)
                         .commit();
-                return true;
-        }
-        else if (item.getItemId() == R.id.menu_daily) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, dailyTasksFragment)
-                    .commit();
             return true;
         }
         else if (item.getItemId() == R.id.menu_event) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.frame_layout, eventRaidFragment)
+                    .commit();
+            return true;
+        }
+        else if (item.getItemId() == R.id.menu_daily) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, dailyTasksFragment)
                     .commit();
             return true;
         }
