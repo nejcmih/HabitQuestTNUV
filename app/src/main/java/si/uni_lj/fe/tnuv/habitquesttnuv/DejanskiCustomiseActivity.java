18663e575b54e61768b243230a05d969a70d00b6
Vito-Verdnik <verdnik.vito@gmail.com>
2024-06-18 23:48:36 +0200

    EUREKA

--- app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/DejanskiCustomiseActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/DejanskiCustomiseActivity.java
@@ -1,12 +1,18 @@
 package si.uni_lj.fe.tnuv.habitquesttnuv;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +22,82 @@ public class DejanskiCustomiseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dejanskicustomise);
+        replaceFragment(new fragment_upperBodyRecycle());
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        ImageButton btnEna = findViewById(R.id.imageView17);
+        btnEna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, fragment_upperBodyRecycle.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+
+        ImageButton btnDva = findViewById(R.id.imageView16);
+        btnDva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, fragment_upperBodyRecycle.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+        ImageButton btnTri = findViewById(R.id.imageView3);
+        btnTri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, fragment_upperBodyRecycle.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+        ImageButton btnStiri = findViewById(R.id.imageView18);
+        btnStiri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, fragment_upperBodyRecycle.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+        ImageButton btnPet = findViewById(R.id.imageView2);
+        btnEna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, fragment_upperBodyRecycle.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+        ImageButton btnSest = findViewById(R.id.imageView4);
+        btnSest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, fragment_upperBodyRecycle.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+
 
 
 
@@ -25,6 +107,9 @@ public class DejanskiCustomiseActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
 //        RecyclerView recyclerView2 = findViewById(R.id.RecyclerUpper);
 //        setUpUpperClothe();
 //        UpperClothe_RecyclewViewAdapter adapter2 = new UpperClothe_RecyclewViewAdapter(this, upperBodyClothes);
@@ -32,5 +117,11 @@ public class DejanskiCustomiseActivity extends AppCompatActivity {
 //        recyclerView2.setLayoutManager(new LinearLayoutManager(this));
 
 
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView2, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/fragment_upperBodyRecycle.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/habitquesttnuv/fragment_upperBodyRecycle.java
@@ -2,11 +2,19 @@ package si.uni_lj.fe.tnuv.habitquesttnuv;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import si.uni_lj.fe.tnuv.habitquesttnuv.Profile.Obleke.UpperClothe;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +22,15 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class fragment_upperBodyRecycle extends Fragment {
+    private RecyclerView recyclerView2;
+        ArrayList<UpperClothe> upperBodyClothes = new ArrayList<>();
+    int[] upperBodyClothesImages = {
+            R.drawable.majica,
+            R.drawable.ph_upper2,
+            R.drawable.ph_upper3,
+            R.drawable.ph_upper4,
+            R.drawable.ph_upper5
+    };
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,6 +70,19 @@ public class fragment_upperBodyRecycle extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+//                    RecyclerView recyclerView2 = findViewById(R.id.RecyclerUpper);
+//            setUpUpperClothe();
+//            UpperClothe_RecyclewViewAdapter adapter2 = new UpperClothe_RecyclewViewAdapter(this.getContext(), upperBodyClothes);
+//            recyclerView2.setAdapter(adapter2);
+//            recyclerView2.setLayoutManager(new LinearLayoutManager(this.getContext()));
+    }
+
+        private void setUpUpperClothe(){
+        String[] upperClotheNames = getResources().getStringArray(R.array.upperClotheName);
+
+        for(int i = 0; i<upperClotheNames.length; i++){
+            upperBodyClothes.add(new UpperClothe(upperBodyClothesImages[i], upperClotheNames[i]));
+        }
     }
 
     @Override
@@ -60,6 +90,19 @@ public class fragment_upperBodyRecycle extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_upper_body_recycle, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        setUpUpperClothe();
+
+        recyclerView2 = view.findViewById(R.id.RecyclerUpper);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        //recyclerView2.setHasFixedSize(true);
+        UpperClothe_RecyclewViewAdapter adapter2 = new UpperClothe_RecyclewViewAdapter(getContext(), upperBodyClothes);
+        recyclerView2.setAdapter(adapter2);
+        adapter2.notifyDataSetChanged();
     }
     
 }
